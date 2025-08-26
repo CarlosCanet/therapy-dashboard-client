dae6d5f440c44a719e1969cc35d9d2064e6fd06b
Carlos Canet <carlos@carlosca.net>
2025-08-26 18:33:56 +0200

    feat: improve useFetch to accept other HTTP Methods

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 
 export type APIResponse<T> = {
@@ -10,7 +10,7 @@ export type APIResponse<T> = {
 };
 
 
-export const useFetch = <T>(url: string): APIResponse<T> => {
+export const useFetch = <T>(method: string, url: string, body?: unknown): APIResponse<T> => {
   const [state, setState] = useState<APIResponse<T>>({
     status: 0,
     statusText: "",
@@ -18,16 +18,28 @@ export const useFetch = <T>(url: string): APIResponse<T> => {
     error: null,
     loading: true,
   });
-  
+
+  const APIMethods: Record<string, () => Promise<AxiosResponse<T>>> = {
+    get: () => axios.get<T>(url),
+    post: () => axios.post<T>(url, body),
+    put: () => axios.put<T>(url, body),
+    patch: () => axios.patch<T>(url, body),
+    delete: () => axios.delete<T>(url)
+  }  
+
   const getData = async () => {
     setState((prevState) => ({ ...prevState, loading: true }));
     try {
-      const response = await axios.get<T>(url);
+      // const response = await axios.get<T>(url);
+      console.log(APIMethods[method])
+      const response = await APIMethods[method.toLowerCase()]();
       let transformedData: T[] | T = response.data;
-      if (response.data && Array.isArray(response.data)) {
-        transformedData = response.data.map(element => {
-          return element.date ? { ...element, date: new Date(element.date) } : element;
-        });
+      if (method.toLowerCase() === "get") {
+        if (response.data && Array.isArray(response.data)) {
+          transformedData = response.data.map(element => {
+            return element.date ? { ...element, date: new Date(element.date) } : element;
+          });
+        }
       }
       console.log("useFetch:", transformedData, "url:", url, "response.data:", response.data);
       setState({
