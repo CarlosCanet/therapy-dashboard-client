837af256e9fb3a1d319dc4cf086a5be61ce00dd3
Carlos Canet <carlos@carlosca.net>
2025-08-26 08:31:11 +0200

    feat: add patient list page

--- src/pages/PatientPages/PatientsListPage.tsx
+++ src/pages/PatientPages/PatientsListPage.tsx
@@ -1,6 +1,26 @@
-function PatientsListPage() {
-  return (
-    <div>PatientsListPage</div>
-  )
+import { ListGroup } from "react-bootstrap";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import type { Patient } from "../../types/types";
+
+interface PatientsListProps {
+  patients: Patient[];
 }
-export default PatientsListPage
+
+function PatientsListPage({patients}: PatientsListProps) {
+  const handleOnChange = () => {
+    console.log("CHANGE!");
+  }
+  return (
+    <div>
+      <h1>Patient List</h1>
+      <FloatingLabel label="Patient name" className="my-3" controlId="patientNameFilter">
+        <Form.Control type="text" placeholder="Search patient" onChange={handleOnChange}/>
+      </FloatingLabel>
+      <ListGroup>
+        {patients.map(patient => <ListGroup.Item>{patient.name}</ListGroup.Item>)}        
+      </ListGroup>
+    </div>
+  );
+}
+export default PatientsListPage;
